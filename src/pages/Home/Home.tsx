e58d1c2c3a4ab3de64983ac6c3a4bd641c0e43bc
Sebastian <sebastian.silva.agurto@hotmail.com>
2023-01-28 07:51:00 -0500

    feat: :sparkles: theme controls added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,19 @@
-import i18next from "i18next";
+import { useCustomDispatch, useCustomSelector } from "@/hooks";
+import { setTheme } from "@/redux/slices/theme";
+import { handleChangeLanguage } from "@/utils";
 import { withTranslation } from "react-i18next";
 import { HomeInterface } from "../interfaces";
 
 const Home: React.FC<HomeInterface> = ({ t }) => {
-  const handleChange = (language: string): void => {
-    i18next.changeLanguage(language);
+  const { theme } = useCustomSelector((state) => state);
+  const dispatch = useCustomDispatch();
+
+  const handleTheme = (): void => {
+    if (theme.theme === "light") {
+      dispatch(setTheme("dark"));
+    } else {
+      dispatch(setTheme("light"));
+    }
   };
 
   return (
@@ -12,22 +21,22 @@ const Home: React.FC<HomeInterface> = ({ t }) => {
       <h1 className="text-4xl font-bold">{t("pages.home.title")}</h1>
       <button
         className="transition-all p-2 bg-blue-300 hover:bg-blue-600 border-blue-300 rounded-md text-white text-xl"
-        onClick={() => handleChange("es")}
+        onClick={() => handleChangeLanguage("es")}
       >
         Español
       </button>
       <button
         className="transition-all p-2 bg-blue-300 hover:bg-blue-600 border-blue-300 rounded-md text-white text-xl"
-        onClick={() => handleChange("en")}
+        onClick={() => handleChangeLanguage("en")}
       >
         English
       </button>
-      {/* <button
+      <button
         className="transition-all p-2 bg-blue-300 hover:bg-blue-600 border-blue-300 rounded-md text-white text-xl"
-        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        onClick={handleTheme}
       >
-        {theme === "light" ? "Dark" : "Light"}
-      </button> */}
+        {theme.theme === "light" ? "Set Dark Theme" : "Set Light Theme"}
+      </button>
     </div>
   );
 };

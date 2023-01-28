badeb971326e40bc1f2d1c7f6274243f312afe0f
Sebastian <sebastian.silva.agurto@hotmail.com>
2023-01-28 07:50:05 -0500

    feat: :sparkles: react-redux and redux-persist was added to the application

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,24 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { I18nextProvider } from "react-i18next";
 
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/lib/integration/react";
+
 import Router from "./router";
 
 import "./styles/index.css";
 import i18n from "./translation";
+import store, { persistor } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <I18nextProvider i18n={i18n}>
-        <Router />
+        <Provider store={store}>
+          <PersistGate persistor={persistor}>
+            <Router />
+          </PersistGate>
+        </Provider>
       </I18nextProvider>
     </BrowserRouter>
   </React.StrictMode>,
